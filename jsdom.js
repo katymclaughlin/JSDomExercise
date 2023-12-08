959868577433a26c787db521e4fbe52a8e39f76b
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2023-12-07 19:40:09 -0500

    "updated selectors to match assignment"

--- jsdom.js
+++ jsdom.js
@@ -1,5 +1,5 @@
 //get header element
-const collection = document.getElementsByTagName("header")
+const collection = document.querySelector("header")
 
 //get all the section elements
 const collection1 = document.getElementsByTagName ("section")
@@ -11,10 +11,10 @@ const collection2 = document.getElementsByClassName ("current")
 const collection3 = collection2[0].nextElementSibling
 
 //get the h2 node from the section before the 'current' section
-const collection4 = collection2[0].previousElementSibling 
+const collection4 = collection2[0].previousElementSibling.childNodes [1]
 
 //get the div that contains the section that has an h2 with a class of 'highlight'
-const collection5 = document.querySelector ("div section h2.highlight")
+const collection5 = document.querySelector("div section h2.highlight").parentElement.parentElement
 
 //get all the sections that contain an H2 (using a single statement);
 const collection6 = document.querySelectorAll ("section h2")
